afdb9fcaa74a042a9efb46c7d7699784674040c2
Eric Vinicius <ericvinicius@linuxmail.org>
2016-08-04 05:56:15 -0700

    added file

---
+++ python/escrever.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+arq = open('/home/vinii/Desktop/lista.txt', 'w')
+texto = """
+Abacate
+Pera
+Melancia
+Abacaxi
+"""
+arq.write(texto)
+arq.close()
