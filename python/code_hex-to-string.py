4739c859ea990e61b708751d34aa531c13399ac2
root <root@debian>
2016-08-04 03:27:26 -0700

    added python

---
+++ python/code_hex-to-string.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Autor: Eric Vinicius (Vinicius Saw)
+# Programa simples apenas com o de incentivar o estudo da linguagem :D
+###############################################################
+def codificar(str_normal):
+    str_hex = str_normal.encode('hex')
+    return(str_hex)
+def decodificar(str_normal):
+    str_hex = str_normal.decode('hex')
+    return(str_hex)
+opcao = 1
+while opcao == 1:
+    print "  \n\nPrograma Str <> Hex - Eric Vinicius"
+    valor = int(input("\n 1 - String -> Hex\n 2 - Hex -> String\n 3 - Sair\n\n Fazer: "))
+    if valor == 1:
+        str_normal = raw_input("\n\nInsira a string:\n")
+        print "String codificada: ", codificar(str_normal)
+    if valor == 2:
+        str_normal = raw_input("\n\nInsira a string:\n")
+        print "String decodificada: ", decodificar(str_normal)
+    if valor == 3:
+        exit()
+
+print "Opcao Incorreta"
+
